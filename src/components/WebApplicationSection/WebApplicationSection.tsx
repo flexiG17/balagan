5182fcae576dfb3eeff3d158edd2fa1eba49ac42
flexiG <mister23krut@gmail.com>
2024-01-23 20:25:56 +0500

    скачивание apk файла по нажатию

--- src/components/WebApplicationSection/WebApplicationSection.tsx
+++ src/components/WebApplicationSection/WebApplicationSection.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './WebApplicationSection.module.scss'
 import HandWithPhoneBackground from "./assets/handWithPhone.png";
+import {Link} from "react-router-dom";
 
 const WebApplicationSection = () => {
     return (
@@ -9,11 +10,11 @@ const WebApplicationSection = () => {
                 <h1 className={styles.leftContent_title}>
                     Полная версия<br/> в приложении
                 </h1>
-                <button className={styles.leftContent_button}>
+                <Link to="/files/android.apk" target="_blank" download className={styles.leftContent_button}>
                     <p className={styles.leftContent_button_text}>
                         Загрузить
                     </p>
-                </button>
+                </Link>
             </div>
             <div className={styles.rightContent}>
                 <img className={styles.rightContent_image} src={HandWithPhoneBackground} alt='Картинка на фон'/>
